99ff0cea9bb1f133f356335bc4e5b9373b7d619b
jungle1987 <karolis.kregzde@gmail.com>
2022-10-18 15:15:29 +0300

    unzipped

---
+++ 1/1.cpp
@@ -0,0 +1,12 @@
+﻿
+
+#include "library.h"
+#include "buffer.h"
+
+
+
+int main()
+{
+    cout << "Greitas variantas (per bufer'į):\n";
+    bufer_nusk("studentai10000.txt", "studentai_copy.txt");
+}
---
+++ 1/buffer.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream> 
+#include <vector> 
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+
+
+void bufer_nusk(std::string read_vardas, std::string write_vardas);
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using std::stringstream;
+using namespace std::chrono;
+using std::ofstream;
