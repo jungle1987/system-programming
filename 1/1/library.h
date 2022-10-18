fbf34c308fbe7991802b5041b562804424c383ff
jungle1987 <karolis.kregzde@gmail.com>
2022-10-18 15:18:03 +0300

    Initial solution version
    Only outputs the data

---
+++ 1/1/library.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+
+
