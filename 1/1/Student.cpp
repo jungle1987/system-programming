cf60a6ffda712f3aa8809f6dfc3acd89b88c3d37
jungle1987 <karolis.kregzde@gmail.com>
2022-11-05 15:04:52 +0200

    v.01 versija
    Programa skaito duomenis ir is failo ir rankiniu budu.

--- 1/1/Student.cpp
+++ 1/1/Student.cpp
@@ -1,13 +1,18 @@
 ﻿
 #include "library.h"
+#include "functions.h"
 
 
 class Student
 {
 private:
-	std::string vardas, pavarde;
+	string vardas, pavarde, eil;
 	vector<int>score_container;
+	vector<Student> duom;
+	
+	
 	int egz = 0;
+
 	double average_scores_formula = 0.;
 	double count_average_formula = 0.;
 
@@ -26,49 +31,141 @@ private:
 public:
 	Student() {};
 
+	Student(const Student& A) {
+
+		this->vardas = A.vardas; 
+		this->pavarde = A.pavarde;
+		this->eil = A.eil;	
+		this->score_container = A.score_container;
+		this->duom = A.duom;
+		this-> egz = A.egz;
+		this-> average_scores_formula = A.average_scores_formula;
+		this-> count_average_formula = A.count_average_formula;
+	}
+
 	Student(string var, string pav, vector<int> sp, int egz) : vardas{ var }, pavarde{ pav }, score_container{ sp }, egz{ egz } {};
 
+	void bufer_nusk(string read_vardas) {
+		stringstream my_buffer;
+		Student Laik;
+		ifstream open_f(read_vardas);
+		my_buffer << open_f.rdbuf();
+		open_f.close();
 
 
+		getline(my_buffer, eil);
+
+		while (my_buffer) {
+			if (!my_buffer.eof()) {
+				getline(my_buffer, eil);
+				stringstream temp_buffer(eil); //sukuria objekta
+				istream_iterator<string> begin(temp_buffer);
+				istream_iterator<std::string> end;
+				vector<std::string> temp_vector(begin, end);// atskirsiu elementus
+				temp_vector.begin();
+				cout << std::endl;
+				temp_vector.end();
+				Laik.vardas = temp_vector[0];// vardo indexas 
+				Laik.pavarde = temp_vector[1];//pavardes indexas
+				for (int i = 2; i <= temp_vector.size() - 2; i++) //nuo antro indexo prasideda pazymiai ir prasuku 7-2 karto.
+				{
+
+					Laik.score_container.push_back(atoi(temp_vector[i].c_str()));//konvertuoju inta i stringa
+
+				}
+
+				Laik.egz = atoi(temp_vector[temp_vector.size() - 1].c_str());
+				duom.push_back(Laik);
+				std::cout << Laik.vardas << "      " << Laik.pavarde << "      ";
+				for (int i = 0; i < Laik.score_container.size(); i++) std::cout << Laik.score_container[i] << "         ";
+				std::cout << Laik.egz << "      ";
+				Laik.score_container.clear();
+				//for (auto a : temp_vector) std::cout << a << std::endl;
+				// splited.push_back(eil);
+
+			}
+			else break;
+		}
+	}
 
 	void deleteStudentScores() {
 		score_container.clear();
 	}
 
+	string getVardas() {
+		return vardas;
+	}
+
+
 	friend std::ostream& operator<<(std::ostream& output, const Student& S) {
 
-
-		output << setw(15) << left << S.pavarde << setw(15) << S.vardas << setw(15) << fixed << setprecision(2) << S.count_average_formula * 1.0 << setw(15) << fixed << setprecision(2) << (double)S.score_container[(double)S.score_container.size() / 2.0] << endl;
+		//cout << setw(15) << left << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis(Vid.) / Galutinis (Med.)" << endl;
+		//cout << "--------------------------------------------------" << endl;
+		output << setw(15) << left << S.pavarde << setw(15) << S.vardas << setw(15) << fixed << setprecision(2) << S.count_average_formula * 1.0 << setw(15) << fixed << setprecision(2) << S.score_container[S.score_container.size() / 2] << endl;
 		
 		return output;
 	}
 
 	friend std::istream& operator>>(std::istream& input, Student& S) {
+		int score = 0;
+
+
+		//string eil;
+		//getline(input, eil);
+		//stringstream my_buffer(eil);
+		
+		input >> S.vardas >> S.pavarde;
+		//cout << S.vardas << S.pavarde << endl;
+		
+		
+		for (int i = 0; i < 7; i++) {
+				
+			input >> score;
+			S.score_container.push_back(score);
+			
+
+			/*my_buffer >> score;
+				cout << score << endl;
+				S.score_container.push_back(score);
+				if (score > 0 && score <= 10) 
+
+				else if (score == 123) { system("CLS"); }
+
+				else {
+					cout << "Klaidingas pazymiu, iveskite skaiciu nuo 0 iki 10 " << endl;
+				} */
+			}
+
+			S.egz = S.score_container.at(S.score_container.size()-1);//paskutini elementa
+			//cout << "test: " << S.score_container.size() << endl;
+			S.score_container.pop_back();//isima paskutini elementa
+			
+			input >> S.egz;
+			S.calculateAverage();
+			S.calculateMedian();
+			//cout << S;
+			//my_buffer.clear();
+			
+			
+		return input;
+	}
+
+		
+
+	/*friend std::istream& operator>>(std::istream& input, Student& S) {
 		int score = 0, score_option = 0;
-		string end_adding_scores;
+
 		cout << "1. Iveskite savo varda: ";
 		input >> S.vardas;
 		system("CLS");
 		cout << "2. Iveskite savo pavarde: ";
 		input >> S.pavarde;
 		system("CLS");
-		cout << "3. Iveskite savo egzamino rezultata: ";
-	RepeatExamInput:
-		input >> S.egz;
-		system("CLS");
-		if (S.egz <= 0 or S.egz > 10)
-		{
-			cout << "Klaida, iveskite skaiciu nuo 1 iki 10" << endl;
-			goto RepeatExamInput;
-
-		}
-
-
-		cout << "4. Pasirinkite opcija pazymiu ivedimui:" << endl;
+		
+		cout << "3. Pasirinkite opcija pazymiu ivedimui:" << endl;
 		cout << "1 Opcija: Skaiciu suvedimas, kai yra zinomi pazymiai. Vartotojas pats suveda pazymius. " << endl;
 		cout << "2 Opcija: Skaiciu suvedimas, kai pazymiai nera zinomi. Programa automatiskai sugeneruos 10 skaiciu. " << endl;
 		cout << "**************************************************************************************************" << endl;
-
 		cout << "Opcija: ";
 
 		cin >> score_option;
@@ -85,7 +182,7 @@ public:
 		else if (score_option == 1) {
 
 			cout << "Noredami sustabdyti pazymiu ivedima, iveskite '123'" << endl;
-			cout << "***************************************************"<< endl;
+			cout << "***************************************************" << endl;
 			cout << "Iveskite pazymius:" << endl;
 
 			while (score != 123) {
@@ -95,12 +192,21 @@ public:
 				else if (score == 123) { system("CLS"); }
 
 				else {
-					cout << "Klaida, iveskite skaiciu nuo 0 iki 10" << endl;
+					cout << "Klaidingas pazymiu, iveskite skaiciu nuo 0 iki 10 " << endl;
 				}
 			};
 
 
+		}
 
+		cout << "4. Iveskite savo egzamino rezultata: ";
+	RepeatExamInput:
+		input >> S.egz;
+		system("CLS");
+		if (S.egz <= 0 or S.egz > 10)
+		{
+			cout << "Klaidingas egzamino rezultatas, iveskite skaiciu nuo 1 iki 10" << endl;
+			goto RepeatExamInput;
 
 		}
 
@@ -110,7 +216,7 @@ public:
 		return input;
 	}
 
-
+	*/
 
 };
 
--- 1/1/main.cpp
+++ 1/1/main.cpp
@@ -1,37 +1,71 @@
-﻿
-
-#include "library.h"
+﻿#include "library.h"
 #include "Student.h"
-#include "Student.cpp"
 
 
 
 int main()
 {
-
+	int data_input_option;
 	vector<Student> list_Students;//vetorius talpins student objektus i sar
 	vector<int>score_container;
+	string eil;
 
-	Student Temp_data("arg", "arg", score_container, 8);//perduodu i inputa
+	Student Temp_data;//perduodu i inputa
 	// nuolat pildomas list_students objektas
-
-	for (int i = 0; i < 2; i++) {
-		cin >> Temp_data;
-		list_Students.push_back(Temp_data);
-		Temp_data.deleteStudentScores();
+	
 
 
-	};
+	
+	cout << "Pasirinkite duomenu ivedimo buda" << endl;
+	cout << "1 Opcija: Duomenu suvedimas rankiniu budu. Vartotojas suveda duomenis rankiniu budu. " << endl;
+	cout << "2 Opcija: Duomenu suvedimas is failo. Vartotojas ikelia faila " << endl;
+	cout << "Opcija: ";
+	cin >> data_input_option;
+	cout << endl;
+	if (data_input_option == 1) {
+		for (int i = 0; i < 2; i++) {
+			cout << endl;
+			cout << "**************************************************************************************************" << endl;
+			cout << "** 1. Iveskite savo varda. " << endl;
+			cout << "** 2. Iveskite savo pavarde. " << endl;
+			cout << "** 3. Iveskite pazymius. " << endl;
+			cout << "** 4. Iveskite savo egzamino rezultata. " << endl;
+			cout << "**************************************************************************************************" << endl;
+			cout << endl;
+			cin >> Temp_data;
+			list_Students.push_back(Temp_data);
+			Temp_data.deleteStudentScores();
 
-	cout << setw(15) << left << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis(Vid.) / Galutinis (Med.)" << endl;
-	cout << "--------------------------------------------------" << endl;
+		};
+	}
 
-	for (int i = 0; i < 2; i++) {
-		cout << list_Students[i] << endl;
-	};
+	
+	
+	else if (data_input_option == 2) {
+		std::ifstream open_f("kursiokai.txt");
+		getline(open_f, eil);
+		while (!open_f.eof()) {
+			open_f >> Temp_data;
 
+			list_Students.push_back(Temp_data);
+			list_Students.begin()->getVardas();
+			Temp_data.deleteStudentScores();
+		}
+	}
 
+	
 
+		cout  << setw(15) << left << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis(Vid.) / Galutinis (Med.)" << endl;
+		cout << "--------------------------------------------------" << endl;
+		//cout << list_Students.size() << endl;
+		
+		for (auto &a : list_Students) {
+			cout << a;
+		};
+
+	
+
+	
 
 	return 0;
 
